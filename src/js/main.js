f5b4cbd16bfc8620a12fa27a4e7dc22770d79a87
Jon <jmcreasman89@gmail.com>
2015-11-02 20:20:31 -0500

    set up most of my functions for instantiating the game, wanted to save my progress so far

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,33 @@
 ;(function() { //IIFE
-  
+
+  var Game = function(canvasId) { //holds all the main game code
+    var canvas = document.getElementById(canvasId); //get the canvas into my game
+    var screen = canvas.getContext('2d'); //var screen will allow me to draw to the canvas. "2d" leads to the creation of a CanvasRenderingContext2D object representing a two-dimensional rendering context.
+    var gameSize = { x: canvas.width, y: canvas.height }; // stores the width and height of the canvas for later use for placing entities on the canvas
+
+    var self = this;
+    var tick = function() { // tick is going to get run about 60 times a second and it's responsible for running all the main game logic
+      self.update();
+      self.draw(screen, gameSize);
+      requestAnimationFrame(tick);
+      // console.log("hello");
+    };
+
+    tick();
+
+  };
+
+  Game.prototype = { //gives Game a prototype
+    update: function() {
+
+    },
+
+    draw: function() {
+
+    }
+  };
+
+  window.onload = function() { //instantiate the game once the DOM is ready with the canvas
+    new Game("screen"); //pass in the id of the canvas I want to write into
+  };
 })(); // End of IIFE
