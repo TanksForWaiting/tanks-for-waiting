bfaf0cca897b4b69d06d902e39cf005103a5221a
William Butts <wbutts1023@gmail.com>
2015-11-04 11:51:08 -0500

    added another simple test

--- test_endpoints.py
+++ test_endpoints.py
@@ -1,14 +1,17 @@
 import requests
 
+
 def test_post():
     p = requests.post('http://localhost:8000/api/players/')
     player_id = p.json()['player_id']
-    g = requests.post('http://localhost:8000/api/games/',data={'player_id':player_id})
+    g = requests.post('http://localhost:8000/api/games/',
+                      data={'player_id': player_id})
     assert p.status_code == 201
     assert g.status_code == 201
     assert len(p.json()['player_id']) == 8
     assert len(g.json()['game_id']) == 8
     assert g.json()['players'][0] == player_id
+
 
 def test_get():
     p = requests.get('http://localhost:8000/api/players/')
