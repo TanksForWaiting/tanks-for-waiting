5a2789bf8d6c49edd217b05eec1142611d28fb48
William Butts <wbutts1023@gmail.com>
2015-11-05 18:44:46 -0500

    adding randomized target locations between 20 and 480 for both x and y

--- tanks_api/api/models.py
+++ tanks_api/api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 import uuid
+import random
 
 # Create your models here.
 
@@ -29,3 +30,11 @@ class Target(models.Model):
     game = models.ForeignKey(Game, null=True, to_field='game_id', related_name='targets')
     x = models.PositiveSmallIntegerField()
     y = models.PositiveSmallIntegerField()
+
+    def save(self,*args, **kwargs):
+        self.x = random.randint(20,480)
+        self.y = random.randint(20,480)
+        super(Target, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return str((self.x, self.y))
