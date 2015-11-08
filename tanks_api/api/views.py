8634243c0c43ac4bf07d5e97a6b4d5c467434674
William Butts <wbutts1023@gmail.com>
2015-11-08 13:09:21 -0500

    targets now check player position in firebase, if player is within 30 units of target, adds a point to the player and destroys the target

--- tanks_api/api/views.py
+++ tanks_api/api/views.py
@@ -61,35 +61,37 @@ class TargetViewSet(viewsets.ModelViewSet):
         player = get_object_or_404(Player, player_id=self.request.data['player_id'])
         game = get_object_or_404(Game, game_id=self.kwargs['games_pk'])
         target = self.get_object()
-        if abs(player.x - target.x) < 30 and abs(player.y - target.y) < 30:
+        r = requests.get("https://tanks-for-waiting.firebaseio.com/games/{}/tanks/{}.json".format(game.game_id, player.player_id))
+        if abs(r.json()['x'] - target.x) < 30 and abs(r.json()['y'] - target.y) < 30:
             player.add_point()
+            requests.delete("https://tanks-for-waiting.firebaseio.com/games/{}/targets/{}.json".format(game.game_id, target.target_id))
             self.perform_destroy(target)
+            t = Target(game=g)
+            t.save()
             return Response(player.score)
         else:
             return Response("nope")
-        # player.add_point()
-            # requests.put('https://tanks-for-waiting.firebaseio.com/games/{}/tanks/{}/score.json'.format(game.game_id, player.player_id), data=str(player.score))
 
-# @receiver(post_save, sender=Game)
-# def put_tanks(sender, **kwargs):
-#     g = kwargs['instance']
-#     p = g.players.first()
-#     if len(g.players.all()) == 0:
-#         pass
-#     else:
-#         requests.put('https://tanks-for-waiting.firebaseio.com/games/{}/tanks/{}/x.json'.format(g.game_id, p.player_id), data=str(p.x))
-#         requests.put('https://tanks-for-waiting.firebaseio.com/games/{}/tanks/{}/y.json'.format(g.game_id, p.player_id), data=str(p.y))
-#         requests.put('https://tanks-for-waiting.firebaseio.com/games/{}/tanks/{}/score.json'.format(g.game_id, p.player_id), data=str(p.score))
-#         while len(g.targets.all()) < 5:
-#             t = Target(game=g)
-#             t.save()
-#
-# @receiver(post_save, sender=Target)
-# def put_targets(sender, **kwargs):
-#     t = kwargs['instance']
-#     g = t.game
-#     if t.game != None:
-#         requests.put('https://tanks-for-waiting.firebaseio.com/games/{}/targets/{}/x.json'.format(g.game_id, t.target_id), data=str(t.x))
-#         requests.put('https://tanks-for-waiting.firebaseio.com/games/{}/targets/{}/y.json'.format(g.game_id, t.target_id), data=str(t.y))
-#     else:
-#         pass
+@receiver(post_save, sender=Game)
+def put_tanks(sender, **kwargs):
+    g = kwargs['instance']
+    p = g.players.first()
+    if len(g.players.all()) == 0:
+        pass
+    else:
+        requests.put('https://tanks-for-waiting.firebaseio.com/games/{}/tanks/{}/x.json'.format(g.game_id, p.player_id), data=str(p.x))
+        requests.put('https://tanks-for-waiting.firebaseio.com/games/{}/tanks/{}/y.json'.format(g.game_id, p.player_id), data=str(p.y))
+        requests.put('https://tanks-for-waiting.firebaseio.com/games/{}/tanks/{}/score.json'.format(g.game_id, p.player_id), data=str(p.score))
+        while len(g.targets.all()) < 5:
+            t = Target(game=g)
+            t.save()
+
+@receiver(post_save, sender=Target)
+def put_targets(sender, **kwargs):
+    t = kwargs['instance']
+    g = t.game
+    if t.game != None:
+        requests.put('https://tanks-for-waiting.firebaseio.com/games/{}/targets/{}/x.json'.format(g.game_id, t.target_id), data=str(t.x))
+        requests.put('https://tanks-for-waiting.firebaseio.com/games/{}/targets/{}/y.json'.format(g.game_id, t.target_id), data=str(t.y))
+    else:
+        pass
