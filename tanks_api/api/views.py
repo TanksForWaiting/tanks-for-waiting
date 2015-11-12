521e5c7b6361bb9fa8c92f55b11a3e94003f71e2
William Butts <wbutts1023@gmail.com>
2015-11-12 12:01:02 -0500

    now deletes a players tank in firebase upon getting a delete request for that player.

--- tanks_api/api/views.py
+++ tanks_api/api/views.py
@@ -162,4 +162,5 @@ class TargetViewSet(viewsets.ModelViewSet):
 def retire_player(sender, **kwargs):
     '''Whenever a player is deleted, create a retired player out of them'''
     player = kwargs['instance']
+    delete(firebase_url + "/games/{}/tanks/{}.json".format(player.game.game_id, player.player_id))
     RetiredPlayer.objects.create(start_time=player.start_time)
